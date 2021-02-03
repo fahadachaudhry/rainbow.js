51f61814836c4c0341578ae361af9d5180294a15
Fahad <shahzada.fahad@yieldwerx.com>
2021-02-04 02:23:31 +0500

    initial commit

---
+++ src/GenerateRainbow.tsx
@@ -0,0 +1,50 @@
+interface IGenerateRainbowProps {
+  totalColors?: number;
+  boxHeight?: string;
+  boxWidth?: string;
+  containerHeight?: string;
+  containerWidth?: string;
+} 
+
+const GenerateRainbow = (props:any) => {
+  const {totalColors, 
+    boxHeight, 
+    boxWidth,
+    containerHeight,
+    containerWidth  ,
+  } = props;
+
+  const colors = [];
+  for (let i = 0; i < totalColors; i+=1) {
+    const color = "hsl(" + (360 * i / totalColors) + ",100%,50%)";
+    colors.push(color);
+  }
+
+  const styles = {
+    height: boxHeight,
+    width: boxWidth
+  }
+
+  return (
+    <div style={{width: containerWidth, height: containerHeight}}>
+      <div className="rainbow-container">
+      {
+        colors.map((item) => (
+          <span style={{ ...styles, backgroundColor: `${item}`}} />
+          ))
+        }
+        </div>
+    </div>
+  );
+
+}
+
+GenerateRainbow.defaultProps = {
+  totalColors: 7,
+  boxHeight: '10px',
+  boxWidth: '10%',
+  containerHeight: "100%",
+  containerWidth: "100%",  
+}
+
+export default GenerateRainbow;
---
+++ src/index.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.scss';
+import GenerateRainbow from './GenerateRainbow';
+
+/// ///////////////////////////////////////////////
+
+interface IInfoBlocks{
+  heading:string; 
+  text:string; 
+  syntax:string;
+  component:any;
+}
+
+const InfoBlocks = (props:IInfoBlocks) => {
+  const {
+    heading,
+    text,
+    syntax,
+    component,
+  } = props;
+  return (
+    <div className="info-block">
+    <div className="flex-50">
+      <h2 className="h1">
+        {heading}
+      </h2>
+      <p>
+        {text}
+      </p>
+      <pre>
+        <code className="language-js">
+        {syntax}
+        </code>
+      </pre>
+    </div>
+    <div className="flex-50">
+    {component}
+    </div>
+  </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GenerateRainbow
+      totalColors={20}
+    />
+    <div className="header">
+      <h1 className="heading">RainbowJS</h1>
+      <p className="tagline">A dead-simple rainbox generator made with React</p>
+    </div>
+    <GenerateRainbow
+      totalColors={20}
+    />
+    <div className="info-container">
+      <InfoBlocks
+        heading="Basic Syntax"
+        text="This is how you do it!"
+        syntax={`
+      <GenerateRainbow
+        totalColors={20}
+      />        
+        `}
+        component={      <GenerateRainbow
+          totalColors={20}
+          />}
+      />
+      <InfoBlocks
+        heading="Customize"
+        text="Try providing heights and widths of the colored blocks. Can be either percent (%) or Pixels (px)"
+        syntax={`
+      <GenerateRainbow
+        totalColors={20}
+        boxHeight="50px"
+        boxWidth="20px"
+      />      
+        `}
+        component={      
+        <GenerateRainbow
+          totalColors={20}
+          boxHeight="50px"
+          boxWidth="20px"
+          />}
+      />
+      <InfoBlocks
+        heading="Customize Container"
+        text="Size of the container can also be customized. Can be either percent (%) or Pixels (px)"
+        syntax={`
+      <GenerateRainbow
+        totalColors={7}
+        boxHeight="50px"
+        boxWidth="20px"
+        containerHeight="100px"
+        containerWidth="100px"
+      />    
+        `}
+        component={      
+        <GenerateRainbow
+          totalColors={7}
+          boxHeight="50px"
+          boxWidth="20px"
+          containerHeight="100px"
+          containerWidth="100px"
+          />
+        }
+      />
+    </div>
+    <p className="tagline">Updates coming soon...</p>
+    <GenerateRainbow
+      totalColors={20}
+    />      
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+/// ///////////////////////////////////////////////
